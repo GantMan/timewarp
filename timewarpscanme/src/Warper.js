01f0db880582d4b54baf4de93334be9c0330d312
Gant Laborde <gantman@gmail.com>
2021-04-10 23:40:30 -0500

    set warper stage

--- timewarpscanme/src/Warper.js
+++ timewarpscanme/src/Warper.js
@@ -1,7 +1,10 @@
 export default function Warper(props) {
   return (
-    <div>
-      <h1>Howdy</h1>
+    <div className="full">
+      <div className="podium">
+        <h1>Howdy</h1>
+        
+      </div>
       <a onClick={props.resetSeen}>RESET</a>
     </div>
   )
